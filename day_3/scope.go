202ffb8ced1ab300a95f37ada768f1dd4dc39567
Shagor Robidas <roysagor88@gmail.com>
2026-07-11 23:58:18 +0600

    day 3

---
+++ day_3/scope.go
@@ -0,0 +1,54 @@
+package main
+import "fmt"
+
+// var name string
+// var age int
+
+// func welcomeName(name string){
+// 	fmt.Println("Welcome ",name)
+// }
+
+// func getName(name string) string{
+// 	fmt.Print("Enter Your Name : ")
+// 	fmt.Scan(&name)
+// 	return name
+// }
+
+// func getAge(age int) int {
+// 	fmt.Print("Enter Your Age: ")
+// 	fmt.Scan(&age)
+// 	return age
+// }
+
+// func displayName(name string){
+// 	fmt.Println("Name: ", name)
+// }
+
+// func displayAge(age int){
+// 	fmt.Println("You are  ", age, "Years Old")
+// }
+
+var(
+	a = 20
+	b = 30
+)
+
+func add(x int, y int){
+	z := x + y
+	fmt.Println("Sum: ",z)
+}
+
+func main(){
+	// name = getName(name)
+	// age = getAge(age)
+	// welcomeName(name)
+	// displayAge(age)
+
+	p := 30
+	q := 40
+	add(p,q) // 70
+	add(a,b) // 50
+	add(a,p) // 50
+	add(b,q) // 70
+	
+}
