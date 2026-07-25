2d7100ce005d253ff1f846e3c96972bd1f94eebf
Shagor Robidas <roysagor88@gmail.com>
2026-07-25 23:24:21 +0600

    Add first order function concepts and logic comments

---
+++ day_5/frist_order_function.go
@@ -0,0 +1,35 @@
+package main
+
+
+/*
+
+	1. perameter vs argument
+	2. First order function
+		i. Standard function or named function
+		ii. Anonymous function
+		iii.IIFE function
+		iv. function expression
+
+	3. Higher order function
+
+
+	functional paradigm -> haskel,  racket
+	 math -> logic (discrete mathematics)
+
+	 1. first order logic
+	 2. higher order logic
+
+
+	 #### LOGIC  ####
+
+	 1. Object (pepole, animal, plants, place, thing) = > Subject
+	 2. Property (Color, Size, Shape, Texture, etc) => Predicate
+	 3. Relation
+
+
+
+
+*/
+
+func operation() {
+	
