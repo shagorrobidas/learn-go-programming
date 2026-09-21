e9e5ba66cac4cfab0d9c92234ee3e19d163ce072
Shagor Robidas <roysagor88@gmail.com>
2026-09-21 21:18:49 +0600

    feat(day_10): demonstrate pointer variable declaration

--- day_10/pointer.go
+++ day_10/pointer.go
@@ -4,6 +4,7 @@ import "fmt"
 func main() {
 	// pointer or address of memory (ram)
 	x := 10
+	p := &x // p is a pointer to x
 	fmt.Println("Value of x:", x)
-	fmt.Println("Address of x:", &x)
+	fmt.Println("Address of x:", p)
 }
