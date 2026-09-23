4458db0482a711efbec596b7d0fd3459f9dbade2
Shagor Robidas <roysagor88@gmail.com>
2026-09-24 01:23:03 +0600

    feat(day_11): add slice literals, capacity, and slicing from slice examples

--- day_11/slice.go
+++ day_11/slice.go
@@ -2,11 +2,28 @@ package main
 import "fmt"
 
 func main() {
-	arr := [6] string{"I", "Love", "Go", "Programming", "Language", "!"}
+	a := []int{10, 20, 30, 40, 50} // slice literal
+	fmt.Println("Slice elements:", a)
+	fmt.Println("Slice Length:", len(a))
+	fmt.Println("Slice Capacity:", cap(a))
+
+	arr := [6]string{"I", "Love", "Go", "Programming", "Language", "!"}
 	fmt.Println("Array elements:", arr)
 
 	s := arr[2:5]
 	fmt.Println("Slice elements:", s)
-	a := arr[1:4]
-	fmt.Println("Another slice elements:", a)
+	s1 := s[1:3]
+	fmt.Println("Slice elements s2 : ", s1)
+	fmt.Println("Slice Length:", len(s1))
+	fmt.Println("Slice Capacity:", cap(s1))
 }
+
+/*
+1. Slice from an existing array
+2. Slice from a slice
+3. slice literal
+
+
+
+
+*/
