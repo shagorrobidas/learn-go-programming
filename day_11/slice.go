bcc5380906dd7991a7d720ca8e2907f69e283299
Shagor Robidas <roysagor88@gmail.com>
2026-09-24 01:40:54 +0600

    feat(day_11): add slice creation using make with length and capacity

--- day_11/slice.go
+++ day_11/slice.go
@@ -2,26 +2,48 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{10, 20, 30, 40, 50} // slice literal
+
+
+	// a := []int{10, 20, 30, 40, 50} // slice literal
+	// fmt.Println("Slice elements:", a)
+	// fmt.Println("Slice Length:", len(a))
+	// fmt.Println("Slice Capacity:", cap(a))
+
+	// arr := [6]string{"I", "Love", "Go", "Programming", "Language", "!"}
+	// fmt.Println("Array elements:", arr)
+
+	// s := arr[2:5]
+	// fmt.Println("Slice elements:", s)
+	// s1 := s[1:3]
+	// fmt.Println("Slice elements s2 : ", s1)
+	// fmt.Println("Slice Length:", len(s1))
+	// fmt.Println("Slice Capacity:", cap(s1))
+
+
+	// s := make([]int, 3)
+	// s[0] = 10
+	// s[2] = 20
+	// fmt.Println("Slice elements:", s)
+	// fmt.Println("Slice Length:", len(s))
+	// fmt.Println("Slice Capacity:", cap(s))
+
+	a := make([]int, 3, 5)
+	a[0] = 10
+	a[1] = 20
+	a[2] = 30	
 	fmt.Println("Slice elements:", a)
 	fmt.Println("Slice Length:", len(a))
 	fmt.Println("Slice Capacity:", cap(a))
 
-	arr := [6]string{"I", "Love", "Go", "Programming", "Language", "!"}
-	fmt.Println("Array elements:", arr)
 
-	s := arr[2:5]
-	fmt.Println("Slice elements:", s)
-	s1 := s[1:3]
-	fmt.Println("Slice elements s2 : ", s1)
-	fmt.Println("Slice Length:", len(s1))
-	fmt.Println("Slice Capacity:", cap(s1))
 }
 
 /*
 1. Slice from an existing array
 2. Slice from a slice
 3. slice literal
+4. make function with len
+5. make function with len and cap
 
 
 
